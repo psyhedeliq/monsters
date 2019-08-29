e1775224e51f0609d019e00e9d08966b5afe55da
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-29 11:28:19 +0100

    Updates

---
+++ precache-manifest.5a4762d94429f4aaf3c5043771f1040d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19897b3eba8059058d02977b5afceb1d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "2a2c408416e7d625ffbe",
+    "url": "/monsters/static/css/main.3558a098.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "2a2c408416e7d625ffbe",
+    "url": "/monsters/static/js/main.9eb0d72f.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
